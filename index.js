9c3f6166e0a0f171cae967bf8921169f4d927959
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2024-09-13 23:46:39 +0300

    first push

---
+++ index.js
@@ -0,0 +1,228 @@
+const colors = {
+  // Black Shades
+  black: [
+    "#E7E7E9",
+    "#CFCFD3",
+    "#A0A0A7",
+    "#71717A",
+    "#45454A",
+    "#18181A",
+    "#141415",
+    "#0F0F10",
+    "#0A0A0B",
+    "#050505"
+  ],
+
+  // Metal Shades
+  metal: [
+    "#F3F4F7",
+    "#E2E4EB",
+    "#CCD0DC",
+    "#B3BACB",
+    "#9BA4BA",
+    "#828DA9",
+    "#626E8E",
+    "#49526A",
+    "#303646",
+    "#191C24"
+  ],
+
+  // Haiti Shades
+  haiti: [
+    "#F4F2F8",
+    "#EAE7F2",
+    "#CDCAD9",
+    "#A7A3B3",
+    "#757281",
+    "#58536E",
+    "#3F395C",
+    "#29244A",
+    "#1A153D",
+    "#151131"
+  ],
+
+  // Purple Shades
+  purple: [
+    "#E6E1FE",
+    "#CEC4FD",
+    "#B3A6F9",
+    "#9C8EF4",
+    "#7A69EE",
+    "#5B4CCC",
+    "#4034AB",
+    "#2A218A",
+    "#1B1472",
+    "#15105B"
+  ],
+
+  // Blue Berry Shades
+  blueBerry: [
+    "#DFE8FF",
+    "#BFD0FF",
+    "#9FB7FF",
+    "#87A3FF",
+    "#5F81FF",
+    "#4561DB",
+    "#2F46B7",
+    "#1E2F93",
+    "#121F7A",
+    "#0E1862"
+  ],
+
+  // Royal Blue Shades
+  royalBlue: [
+    "#D9E6FD",
+    "#B4CBFC",
+    "#8DACF6",
+    "#6E91EC",
+    "#4169E1",
+    "#2F50C1",
+    "#203AA2",
+    "#142782",
+    "#0C1A6C",
+    "#0A1557"
+  ],
+
+  // Sky Shades
+  sky: [
+    "#CEFDFE",
+    "#9DF4FE",
+    "#6CE5FE",
+    "#47D2FE",
+    "#0BB4FE",
+    "#088CDA",
+    "#0569B6",
+    "#034A93",
+    "#023579",
+    "#022A5F"
+  ],
+
+  // Turquoise Shades
+  turquoise: [
+    "#CCFDF3",
+    "#9AFBEF",
+    "#67F5EF",
+    "#41E6EC",
+    "#08C7E0",
+    "#059CC0",
+    "#0475A1",
+    "#025481",
+    "#013D6B",
+    "#013156"
+  ],
+
+  // Persian Green Shades
+  persianGreen: [
+    "#CAFAE4",
+    "#97F5D2",
+    "#5FE2BE",
+    "#37C6AB",
+    "#06A192",
+    "#048A89",
+    "#036A73",
+    "#014D5D",
+    "#01384D",
+    "#012C3C"
+  ],
+
+  // Pastel Green Shades
+  pastelGreen: [
+    "#D2FBD0",
+    "#A3F7A9",
+    "#72E985",
+    "#4DD470",
+    "#1CB854",
+    "#149E53",
+    "#0E8450",
+    "#086A49",
+    "#055843",
+    "#044837"
+  ],
+
+  // Grass Shades
+  grass: [
+    "#E3FAD6",
+    "#C2F6AE",
+    "#93E380",
+    "#66C95B",
+    "#2FA52D",
+    "#208D28",
+    "#167625",
+    "#0E5F21",
+    "#084F1F",
+    "#074119"
+  ],
+
+  // Carrot Shades
+  carrot: [
+    "#FFF3D5",
+    "#FFE4AB",
+    "#FFD181",
+    "#FFBE62",
+    "#FFA02E",
+    "#DB7E21",
+    "#B75F17",
+    "#93450E",
+    "#7A3108",
+    "#602706"
+  ],
+
+  // Orange Shades
+  orange: [
+    "#FFEAD2",
+    "#FFCEA6",
+    "#FFAD7A",
+    "#FF8C59",
+    "#FF5722",
+    "#DB3918",
+    "#B72111",
+    "#930E0A",
+    "#7A060B",
+    "#610509"
+  ],
+
+  // Red Shades
+  red: [
+    "#FEE3D4",
+    "#FDC0AB",
+    "#FB9580",
+    "#F86D60",
+    "#F42D2D",
+    "#D12030",
+    "#AF1631",
+    "#8D0E2F",
+    "#75082E",
+    "#5F0726"
+  ],
+
+  // Raspberry Shades
+  raspberry: [
+    "#FED9D5",
+    "#FEACAD",
+    "#FD838F",
+    "#FB6382",
+    "#F9316D",
+    "#D6236A",
+    "#B31865",
+    "#900F5C",
+    "#770955",
+    "#5F0743"
+  ],
+
+  // Fuchsia Shades
+  fuchsia: [
+    "#FECCEC",
+    "#FE99E3",
+    "#FE66E3",
+    "#FE40EC",
+    "#FE01FC",
+    "#C900DA",
+    "#9800B6",
+    "#6E0093",
+    "#500079",
+    "#410061"
+  ]
+};
+
+// Export the colors object so it can be imported by developers
+module.exports = colors;
